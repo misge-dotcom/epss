f54eac6d5adf26bb3890552ae7f3e7afb7f6466d
misge-dotcom <misge.intra@gmail.com>
2023-12-30 19:04:19 +0100

    Files updated and progess bar added

--- epss_api/download.py
+++ epss_api/download.py
@@ -7,6 +7,7 @@ import numpy as np
 from pandas import json_normalize
 import json
 from collections.abc import Iterable
+from alive_progress import alive_bar
 
 url = "https://api.first.org/data/v1/epss?percentile-gt=0.95&epss-gt=0.95&pretty=true"
 max_offset = 160000
@@ -36,10 +37,11 @@ def fetch_data(offset):
     if response.status_code == 200:
         json_data = response.json()
         data = json_data.get("data",[])
-        total = json_data.get('total', 0)
-        if total== offset:
+        total = json_data.get("total", [0])
+        remoteOffset = json_data.get("offset", [0])
+        if int(total) <= int(offset):
             print("---=== Offset reached ===---")
-            return
+            return None, remoteOffset
         if not data:  # Check if data is empty
             return None
         return data, total
@@ -50,52 +52,27 @@ def fetch_data(offset):
 
 start_time = time.time()
 results = []
+
 with ThreadPoolExecutor(max_workers=max_threads) as executor:
     while current_offset <= max_offset:
-        data = fetch_data(current_offset)
-        if data:
-            #token = json.dumps(data[0])
-            #token = token.replace('\'','\"')
-            results.append(data[0])
-            current_offset += offset_increment 
-            # Update the offset using the total count from JSON
-        else:
-            print("Empty data received or failed request. Stopping iteration.")
-            break  # Stop the iteration if empty data is received or request fails
+        print("Gettting the data for:",current_offset)
+        data, total = fetch_data(current_offset)
+        with alive_bar(total, title="Getting EPSS Data...", ctrl_c=False) as bar:
+            
+            if data:
+                #token = json.dumps(data[0])
+                #token = token.replace('\'','\"')
+                results.append(data[0])
+                current_offset += offset_increment 
+                bar(current_offset)
+                # Update the offset using the total count from JSON
+            else:
+                print("Empty data received or failed request. Stopping iteration.")
+                break  # Stop the iteration if empty data is received or request fails
 
 # Writing only the 'data' portion to a CSV file
 with open('responses.json', 'w', encoding='utf-8') as csvfile:
-    #writer = csv.writer(csvfile)
-    #writer.writerows(results)
-   # writer.write(json.dumps(json.loads(results), indent=4, sort_keys=True))
     json.dump(flatten(results),csvfile, indent=4, sort_keys=True)
-    #writer.close()
-    
-'''
-results = []
-with ThreadPoolExecutor(max_workers=max_threads) as executor:
-    while current_offset <= max_offset:
-        print("Offset ", current_offset, max_offset)
-        data = fetch_data(current_offset)
-        #print(data)
-        if data:
-            results.append(pd.DataFrame.from_records(data[0]))
-            print("========================")
-            print(pd.DataFrame.from_records(data[0]))
-            print("========================")
-            #for entry in data:
-            #    results.append(pd.DataFrame.from_records(entry))
-                #break
-            #results.extend(data)
-            current_offset += offset_increment 
-            #print ("Working ...",current_offset)
-            #current_offset += total  # Update the offset using the total count from JSON
-        else:
-                print("Empty data received. Stopping iteration.")
-                executor.shutdown( cancel_futures = True )
-                print("executor.shutdown")
-                break  # Stop the iteration if empty data is received
-'''
     
 end_time = time.time()
 elapsed_time = end_time - start_time
